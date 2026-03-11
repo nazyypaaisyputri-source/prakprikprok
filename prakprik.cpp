287d29c42b1fd167b4063c8fcb63d911b1bb5e09
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-03-11 10:58:36 +0700

    menambahkan prosedur masukanData untuk input berat dan tinggi

--- prakprik.cpp
+++ prakprik.cpp
@@ -9,3 +9,10 @@ int main() {
 int main() {
     return 0;
 }
+
+void masukanData(float &berat, float &tinggi) {
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
